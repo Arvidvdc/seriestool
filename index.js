904af7b9c98bde994afb4513af80ea848c835ee9
Arvid van de Crommert <arvidvdc@gmail.com>
2019-12-10 21:13:21 +0100

    Added header and footer partials.

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ require('dotenv').config();
 
 // Express variables
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "./public"));
+app.use(express.static(__dirname + "/public"));
+
+app.use((req,res,next)=>{
+    res.locals.currentUser=req.user;
+    next();
+});
 
 // Routes
 app.use(indexRoutes);
