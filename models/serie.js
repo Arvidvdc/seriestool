22f877337c84fe6b9ff7d809c857b1d549ea31a5
Arvid van de Crommert <arvidvdc@gmail.com>
2019-12-10 10:47:50 +0100

    Added database schemas.

---
+++ models/serie.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const serieSchema = new mongoose.Schema({
+    title: String,
+    plot: String,
+    genre: String,
+    subs: String,
+    actors: String,
+    director: String,
+    classification: String,
+    ratingIMDb: Number,
+    url: String,
+    country: String,
+    language: String,
+    runningTime: Number,
+    airDate: Date,
+    Status: String,
+    currentSeason: Number,
+    nextAirDate: Date,
+    sizeOnDisk: Number,
+    locationOnDisk: String,
+    seenAllEpisodes: Boolean,
+    registredInKodi: Boolean,
+    searchSubs: Boolean,
+    personalNotes: String,
+    DateAdded: Timestamp,
+    DateChanged: Timestamp,
+    owner: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        username: String
+    },
+});
+
+module.exports = mongoose.model("Serie", serieSchema);
