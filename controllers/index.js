0a5839577c4f93694f69b1b16dff64fdb718892e
Arvid van de Crommert <arvidvdc@gmail.com>
2019-12-10 22:38:02 +0100

    Created gallery on home.ejs.

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,15 @@
+const   fs      = require("fs");
+
 // Default
 exports.default = (req,res)=>{
-    res.render("./default/home");
+    let path="public/images"
+    let server="http://" + process.env.APP_IP + ":" + process.env.APP_PORT + "/" + path
+        fs.readdir(path,(err,imageList)=> {
+        if(!err){
+            res.render("./default/home",{path: "/images", imageList: imageList});
+        } else {
+            console.log("Error");
+            res.send("Error" + err)
+        }
+    });
 }
