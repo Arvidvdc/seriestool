07ac0ac1324514c27b9e5c0c81c1c0155e194a8a
Arvid van de Crommert <arvidvdc@gmail.com>
2019-12-19 04:55:52 +0100

    Added welcome-message.

--- controllers/index.js
+++ controllers/index.js
@@ -6,6 +6,7 @@ exports.default = (req,res)=>{
     let server="http://" + process.env.APP_IP + ":" + process.env.APP_PORT + "/" + path
         fs.readdir(path,(err,imageList)=> {
         if(!err){
+            req.flash("success", "Welkom!");
             res.render("./default/home",{path: "/images", imageList: imageList, page: "home", css: "css/main.css", buttons: "css/buttons.css"});
         } else {
             req.flash("error", "Error while loading images." + err.message);
